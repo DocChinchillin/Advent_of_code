f05a31f545456e311e56ed113effe2339c51ca02
Rok Švikart <docchinchillin@gmail.com>
2021-12-04 12:45:18 +0100

    day 4

---
+++ 2021/04/p1.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def main():
+    with open("input.txt") as f:
+    	lines = f.read().splitlines()
+    
+    bingos = lines[0].split(",")
+
+    boards = []
+    
+    temp = []
+    lines[0] = ""
+    for line in lines:
+        if line == "":
+            if(len(temp)!=0):
+                boards.append(temp)
+                temp = []
+        else:
+            temp.append(line.split()) 
+    
+    boards=np.array([np.array(xi) for xi in boards])
+    
+    for bingo_num in bingos:
+        temp = np.empty_like(boards)
+
+        for i,bord in enumerate(boards):
+            temp[i]=np.where(bord == bingo_num, "-1", bord)
+
+        boards = temp
+        win = checkWin(boards)
+
+        if(win):
+            break
+
+    print(win * int(bingo_num))
+
+def checkWin(boards):
+    for i in range(0,len(boards)):
+        for j in range(0,len(boards[i])):
+            if np.count_nonzero(boards[i][:,j] == "-1") == len(boards[i]) or np.count_nonzero(boards[i][j,:] == "-1") == len(boards[i]):
+                return calcWin(boards[i])
+                
+    return 0
+            
+def calcWin(board):
+    board = np.where(board == "-1", "0", board)
+    board = board.astype(int)
+    vsota = sum(sum(board))
+
+    return vsota
+
+if __name__ == "__main__":
+    main()
---
+++ 2021/04/p2.py
@@ -0,0 +1,67 @@
+import numpy as np
+import time
+
+def main():
+    with open("input.txt") as f:
+    	lines = f.read().splitlines()
+    
+    bingos = lines[0].split(",")
+
+    boards = []
+    
+    temp = []
+    lines[0] = "" 
+    for line in lines:
+        if line == "":
+            if(len(temp)!=0):
+                boards.append(temp)
+                temp = []
+        else:
+            temp.append(line.split()) 
+    
+    boards = np.array([np.array(xi) for xi in boards])
+    
+    end = 0
+
+    for bingo_num in bingos:
+        temp = np.empty_like(boards)
+
+        for i,bord in enumerate(boards):
+            temp[i] = np.where(bord == bingo_num, "-1", bord)
+
+        boards = temp
+        
+        if(len(boards) == 1):
+            for i in range(0,len(boards[0])):
+                if np.count_nonzero(boards[0][:,i] == "-1") == len(boards[0]) or np.count_nonzero(boards[0][i,:] == "-1") == len(boards[0]):
+                    end = 1
+                    break
+            if end:
+                break
+        boards = checkWin(boards)
+
+    win = calcWin(boards[0])
+
+    print(int(bingo_num) * win)
+
+    
+
+def checkWin(boards):
+    temp = []
+    
+    for i in range(0,len(boards)):
+        for j in range(0,len(boards[i])):
+            if np.count_nonzero(boards[i][:,j] == "-1") == len(boards[i]) or np.count_nonzero(boards[i][j,:] == "-1") == len(boards[i]):
+                temp.append(i)
+
+    return np.delete(boards,temp,0)
+            
+def calcWin(board):
+    board = np.where(board == "-1", "0", board) 
+    board = board.astype(int)
+    vsota = sum(sum(board))
+
+    return vsota
+
+if __name__ == "__main__":
+    main()
