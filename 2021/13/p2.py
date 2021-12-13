0b455609ef05cc4e4e7382d5b462dd5902e6e716
Rok Švikart <docchinchillin@gmail.com>
2021-12-13 13:30:11 +0100

    add fancy output

--- 2021/13/p2.py
+++ 2021/13/p2.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 
 def main():
-    with open("test") as f:
+    with open("input") as f:
         lines = f.read().splitlines()
    
     sp = lines.index("")
@@ -13,7 +13,7 @@ def main():
     for fold in folds:
         temp.append(fold.strip("fold along "))
     folds = temp
-    
+
     y = None
     x = None
     i = 0
@@ -43,10 +43,20 @@ def main():
             mat = np.delete(mat,index,1) 
             ar1,ar2 = np.hsplit(mat,2)
             mat = ar1 + np.flip(ar2,1)
+     
+    mat[mat > 0] = 1
+    
+    vrstica = ["","","","","",""]
+    for crka in np.hsplit(mat.astype(int),8):
+        i = 0
+        for row in crka:
+            r = ["#" if i==1 else " " for i in row.tolist()]
+            row = "".join(r)
+            vrstica[i]+= row + " "
+            i+=1
 
-    mat[mat>0] = 1
-    print(mat.astype(int))
-
+    for v in vrstica:
+        print(v)
 
 if __name__ == "__main__":
     start = time.time()
