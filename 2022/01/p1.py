7c8e29def2c99f34741d9b1add84a53d7a37ccac
Doc <docchinchillin@gmail.com>
2022-12-01 16:22:03 +0100

    1 nal

---
+++ 2022/01/p1.py
@@ -0,0 +1,22 @@
+import time
+
+def main():
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+    sum = [0]
+    elf = 0
+    for line in lines:
+        if line == '':
+            elf+=1
+            sum.append(0)
+            continue
+        sum[elf] += int(line)
+    
+    print(max(sum))
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print(f"Time is : {end - start}")
---
+++ 2022/01/p2.py
@@ -0,0 +1,22 @@
+import time
+
+def main():
+    with open('input.txt') as f:
+        lines = f.read().splitlines()
+
+    sums = [0]
+    elf = 0
+    for line in lines:
+        if line == '':
+            elf+=1
+            sums.append(0)
+            continue
+        sums[elf] += int(line)
+    
+    print(sum(sorted(sums,reverse=True)[:3]))
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print(f"Time is : {end - start}")
