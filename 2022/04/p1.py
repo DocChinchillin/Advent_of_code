2948131d5b19a88a208c23292e1c6fa192c10362
Doc <docchinchillin@gmail.com>
2022-12-04 08:23:39 +0100

    Day 4

---
+++ 2022/04/p1.py
@@ -0,0 +1,39 @@
+import time
+
+def main():
+
+    with open('input') as f:
+        lines = f.read().splitlines()
+
+    count = 0
+
+    for line in lines:
+        a,b = line.split(',')
+
+        aMin,aMax = [int(a) for a in a.split('-')]
+        bMin,bMax = [int(b) for b in b.split('-')]
+
+        # a contained in b
+        if bMin <= aMin and aMax <= bMax:
+            count += 1
+            continue
+        # b contained in a
+        if aMin <= bMin and bMax <= aMax:
+            count += 1
+            continue
+        
+
+
+
+
+    
+    
+    print(count)
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print(f"Time is : {end - start}")
